12dc4cbf842775304b7b247959adb5a4cfe1ecf1
zhangly <dear531@21cn.com>
2015-04-26 18:20:59 +0800

    append usage of enum

--- reference_enum.c
+++ reference_enum.c
@@ -10,9 +10,14 @@ int main(int argc, char *argv[])
 	};
 	int value[MAX] = {1, 0, 1024};
 	int i;
+	enum enum_value num;
 	for (i = 0; MAX > i; i++) {
 		fprintf(stdout, "enum_value.OEN:%d\n", (enum enum_value)i);
 		fprintf(stdout, "value :%d\n", value[(enum enum_value)i]);
+		num = (enum enum_value)i;
+		fprintf(stdout, "num:%d\n", num);
 	}
+	num = i;
+	fprintf(stdout, "num:%d\n", num);
 	return 0;
 }
