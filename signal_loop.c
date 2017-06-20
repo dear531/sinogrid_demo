324087d3faa6fcdc08542635a3d132e71c37a5c1
zhangliuying <dear531@21cn.com>
2017-06-20 17:15:29 +0800

    call myself for signal

--- signal_loop.c
+++ signal_loop.c
@@ -7,6 +7,9 @@ void sig_alarm(int signum)
 {
 	write(STDOUT_FILENO, ".", strlen("."));
 	alarm(2);
+	write(STDOUT_FILENO, "2", strlen("2"));
+	sleep(3);
+	write(STDOUT_FILENO, "3", strlen("3"));
 }
 int main(void)
 {
@@ -16,8 +19,10 @@ int main(void)
 #endif
 	signal(SIGALRM, sig_alarm);
 	alarm(2);
-	while (1)
+	while (1) {
 		sleep(10);
+		write(STDOUT_FILENO, "+", strlen("+"));
+	}
 	fprintf(stdout, "here is appear ?\n");
 	return 0;
 }
